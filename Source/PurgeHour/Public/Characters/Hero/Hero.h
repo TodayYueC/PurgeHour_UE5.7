c88ae03f880ce4ebf997ba918764e24c030e4ae4
YueC <Roxiyetto@163.com>
2026-01-05 14:42:08 +0800

    Feat: 角色资源导入

---
+++ Source/PurgeHour/Public/Characters/Hero/Hero.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Characters/CharacterBase.h"
+#include "Hero.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PURGEHOUR_API AHero : public ACharacterBase
+{
+	GENERATED_BODY()
+	
+};
